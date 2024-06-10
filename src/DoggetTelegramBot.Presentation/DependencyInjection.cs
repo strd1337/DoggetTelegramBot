72bb6f541f94024304d4b8d79e75b6d0cce805d7
StrdL <stas20001234567@mail.ru>
2024-06-11 02:05:15 +0300

    Set up the project

---
+++ src/DoggetTelegramBot.Presentation/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿namespace DoggetTelegramBot.Presentation
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPresentation(
+            this IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddEndpointsApiExplorer();
+
+            services.AddSwaggerGen();
+
+            return services;
+        }
+    }
+}
