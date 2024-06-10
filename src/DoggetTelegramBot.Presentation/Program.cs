dc3ee397481db50ad3e814480ac7765acc8fe43e
StrdL <97736243+strd1337@users.noreply.github.com>
2024-06-11 02:17:54 +0300

    Set up the project (#1)

--- src/DoggetTelegramBot.Presentation/Program.cs
+++ src/DoggetTelegramBot.Presentation/Program.cs
@@ -1,15 +1,16 @@
+using DoggetTelegramBot.Presentation;
+using DoggetTelegramBot.Application;
+using DoggetTelegramBot.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services
+    .AddPresentation()
+    .AddApplication()
+    .AddInfrastructure();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,8 +18,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
